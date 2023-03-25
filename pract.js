aada9a6ee308b7bc96df003d1e0cf42a649bd0ba
FAncciPIxels <121621676+FAncciPIxels@users.noreply.github.com>
2023-03-24 17:20:13 -0700

    Add files via upload

---
+++ pract.js
@@ -0,0 +1,20 @@
+
+function clicked() {
+    let subAr = ["red", "green", "yellow", "black", "grey", "purple", "blue"]
+    let inputed = document.getElementById("inputed").value;
+    //sub =document.getElementById("sub");
+    var outPuted  =document.getElementById("outPut"),
+    neg  = document.getElementById("neg");
+    let r = 150;
+   // let atrr = document.createAttribute("style");
+    outPuted.style.backgroundColor = inputed;    
+    outPuted.classList.add("show")
+     if (outPuted.style.background = ) {
+        outPuted.classList.remove("show")
+     }
+     else {
+        outPuted.classList.add("show")
+     }
+
+     
+}
